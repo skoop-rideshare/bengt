7b91bafc0fa1cfab81b7e6373046ce717e497707
Anton Björn Ivarsson <anton.ivarsson@sellpy.se>
2019-07-07 13:56:44 +0200

    Add mongoose for easier mongodb handling. Add passport.js and auth handling for protected routes + salt and hash for passes.

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
 const app = express();
 
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-//
-// ROUTERS ARE ADDED HERE
-// 
-app.use('/', indexRouter);
-app.use('/user', userRouter);
+if(!isProduction) {
+  app.use(errorHandler());
+}
 
-// STANDARD ERROR HANDLERS
-app.use((req, res, next) => {
-  next(createError(404));
-});
+//Configure Mongoose
+require('./db')
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
   res.status(err.status || 500);
-  res.render('error');
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
-module.exports = app;
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
--- db.js
+++ db.js
@@ -1,17 +1,21 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const mongoose = require('mongoose');
+const server = process.env.MONGO_URI;
 
-const url = process.env.MONGO_URI
-
-const initMongo = async () => {
-	let db
-	try {
-		db = await MongoClient.connect(url)
-	} catch(error) {
-		return console.log('Could not connect to mongoDB', error)
-	}
-	console.log('Connected to: ', url)
-	return db
+class Database {
+  constructor() {
+    this._connect()
+  }
+  
+_connect() {
+	console.log(server)
+     mongoose.connect(server)
+       .then(() => {
+         console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error')
+       })
+  }
 }
 
-module.exports = initMongo
+module.exports = new Database()
