4fad188c31482cc7c3b4f560fa5d5f6536f4df05
JoelSoderberg <soderberg.joel@gmail.com>
2019-08-21 15:55:15 +0200

    bla

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,15 +65,16 @@ router.post('/login', auth.optional, (req, res, next) => {
     if (err) {
       return next(err)
     }
-
     if (passportUser) {
       const user = passportUser
       user.token = passportUser.generateJWT()
-
       return res.json({ user: user.toAuthJSON() })
     }
-
-    return res.status(400).info
+    return res.status(400).json({
+      errors: {
+        user: 'does not exist'
+      }
+    })
   })(req, res, next)
 })
 
